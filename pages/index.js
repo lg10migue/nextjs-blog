7560d3d5e050bef6624c6a90ac11614a6608d645
lg10migue <lop18028@byui.edu>
2023-11-15 21:52:48 -0700

    Update introduction.
    Added more information in my introduction.

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
                 <title>{siteTitle}</title>
             </Head>
             <section className={utilStyles.headingMd}>
-                <p>Hello, I'm Miguel. I'm a Software Engineer learning NextJS.</p>
+                <p>Hello, I'm Miguel. I'm a Software Engineer learning NextJS. I really love to play soccer⚽!</p>
                 <p>
                     (This is a sample website - you’ll be building a site like this on{' '}
                     <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
